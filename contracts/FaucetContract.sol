e765da4b1028ed5132da3cbebe38d023e6700afa
alexprimak58 <aprimak43@gmail.com>
2024-04-04 15:42:17 +0300

    Faucet

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+  //storage variables
+  uint public funds = 1000; //positive values
+  int public counter = 10;
+
+}
+
