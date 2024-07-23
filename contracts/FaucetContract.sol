20e23eb7388bae0f76963f9c80c760f72532a7ba
alexprimak58 <aprimak43@gmail.com>
2024-07-23 13:37:08 +0300

    mapping with duplicates

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,21 +3,36 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-  address[] public funders;
+  uint public numOfFunders;
+  mapping(uint => address) public funders;
 
   receive() external payable {}
 
   function addFunds() external payable {
-  funders.push(msg.sender);
+    uint index = numOfFunders++;
+    funders[index] = msg.sender;
   }
 
-  function getAllFunders() public view returns (address[] memory) {
-    return funders;
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+
+    for(uint i=0; i < numOfFunders; i++) {
+      _funders[i] = funders[i];
+    }
+    return _funders;
   }
 
   function getFunderAtIndex(uint8 index) external view returns(address) {
-    address[] memory _funders = getAllFunders();
-    return _funders[index];
+    // address[] memory _funders = getAllFunders();
+    return funders[index];
   }
 }
 
+//open console
+// truffle console
+//create an instance
+// const instance = await Faucet.deployed();
+// add funds to the contract
+// instance.addFunds({from: accounts[3],value: "20000000"})
+// get a third address that deposited to the faucet
+// instance.getFunderAtIndex(2)
