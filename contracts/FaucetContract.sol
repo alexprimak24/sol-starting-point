127d8d3549cab35d7f5f2f1d14e5313b47d87e07
alexprimak58 <aprimak43@gmail.com>
2024-07-23 14:15:02 +0300

    mapping without duplicates

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,27 +4,34 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
   uint public numOfFunders;
-  mapping(uint => address) public funders;
-
+  mapping(address => bool) public funders;
+  mapping(uint => address) public lutFunders;
+  
   receive() external payable {}
 
   function addFunds() external payable {
-    uint index = numOfFunders++;
-    funders[index] = msg.sender;
+    address funder = msg.sender;
+    if (!funders[funder]){
+      uint index = numOfFunders++; //why this works?
+      //numOfFunders++ will increment the value of numOfFunders by 1
+      //BUT it returns the value before the increment!
+      lutFunders[index] = funder;
+      funders[funder] = true;
+    }
   }
 
   function getAllFunders() external view returns (address[] memory) {
     address[] memory _funders = new address[](numOfFunders);
 
     for(uint i=0; i < numOfFunders; i++) {
-      _funders[i] = funders[i];
+      _funders[i] = lutFunders[i];
     }
     return _funders;
   }
 
   function getFunderAtIndex(uint8 index) external view returns(address) {
     // address[] memory _funders = getAllFunders();
-    return funders[index];
+    return lutFunders[index];
   }
 }
 
