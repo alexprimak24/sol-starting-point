94f60bd365a89823bf827ae0a73252eac4fd7709
Alexander <78500984+alexprimak58@users.noreply.github.com>
2024-08-01 13:39:28 +0300

    Update SummerPractice.sol

--- contracts/SummerPractice.sol
+++ contracts/SummerPractice.sol
@@ -157,3 +157,129 @@ contract Demo {
     function receiveFunds() public payable {
         payments[msg.sender] = msg.value;
     }
+
+
+//DAY 3
+
+//ARRAY, BYTES, ENUM, STRUCT
+
+contract Demo {
+    //Enum
+    enum Status {
+        Paid,Delivered,Received
+    }
+    // //Status is data type of currentStatus
+    // //we set not the value Paid,Delivered..., we receive Index of them Paid - 0, Delivered - 1
+    // Status public currentStatus;
+
+    function pay() public {
+        currentStatus = Status.Paid;
+    }
+    function delivered() public {
+        currentStatus = Status.Delivered;
+    }
+
+    //Array with set length
+    //uint - elements, 10 - max number of elements
+    uint[10] public items = [1,2,3];
+
+    function demo() public {
+        items[0] = 100;
+        items[2] = 200;
+        items[4] = 400;
+
+    }
+
+    //Nested array
+    //2el in the outer array
+    //3el in the inner array
+    uint[3][2] public items;
+
+    function demo() public {
+       items = [
+        [1,2,3],
+        [3,4,5]
+       ];
+       //to call 4 we need to call 1,1
+    }
+
+    //Array with dynamic length
+
+    uint[] public items;
+    uint public len;
+    function demo() public {
+        //push exist only for dynamic arrays
+        items.push(4);//add first el
+        items.push(5);//add sencond el
+        //we can measure the length of an array
+        len = items.length;
+    }
+
+    // //We can also create a temporary arrays that exist only while function execution
+    function sampleMemory() public pure returns(uint[] memory) {
+        uint[] memory tempArray = new uint[](10);
+        tempArray[0] = 1;
+        return tempArray;
+    }
+
+    //Byte Array
+    //from 1 --> 32 bytes
+    //32*8 - also max length 256 bits as well as for uint
+    //it takes 1 byte or 8bits
+    //we can even input strings here - but it will be converted to bytes
+    bytes32 public myVar = "test";
+    //dynamic byte arr
+    bytes public myVarDynamic = "test";
+
+    // //how to store other that english
+    bytes public myVarDynamicUkr = unicode"Привіт!";
+
+    function demo() public view returns(uint) {
+        //in that case all good and we receive 4, but in case we paste here not english
+        //but symbols from other languages, everything becomes more complicated
+        return myVarDynamic.length;
+    }
+
+    function demo() public view returns(bytes1) {
+        //we can get the first byte, btw then we can decode it and get what symbol we received
+        return myVarDynamic[0];
+    }
+
+    //Struct - like Objects in JS
+    struct Payment {
+        uint amount;
+        uint timestamp;
+        address from;
+        string message;
+        //this is not allowed
+        // Payment payment;
+    }
+
+    struct Balance {
+        uint totalPayments;
+        mapping(uint => Payment) payments;
+    }
+
+    mapping(address => Balance) public balances;
+
+    function getPayment(address _addr, uint _index) public view returns(Payment memory){
+        return balances[_addr].payments[_index];
+    }
+
+    function pay(string memory message) public payable {
+
+        uint paymentNum =  balances[msg.sender].totalPayments;
+
+        //uint totalPayments - incremented
+        balances[msg.sender].totalPayments++;
+        //as for now it is temporary 
+        Payment memory newPayment = Payment(
+             msg.value,
+             block.timestamp,
+             msg.sender,
+             message   
+        );
+        //and now we stored it in the blockchain
+        balances[msg.sender].payments[paymentNum] = newPayment;
+    }
+}
